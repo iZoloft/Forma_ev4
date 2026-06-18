6c2c791516fcf6e794dd0269c872b962d9257379
Ariel <akoppg@gmail.com>
2026-06-18 16:59:20 -0400

    Terminado??

--- functions.py
+++ functions.py
@@ -15,7 +15,7 @@ def _validar_str(string):
         print("Campo no puede venir vacio")
 
 def _validar_int(numero):
-    if numero > 0:
+    if numero >= 0:
         return True
     else:
         print("El número debe ser mayor a 0")
@@ -26,22 +26,41 @@ def _validar_float(numero):
     else:
         print("El número debe ser mayor a 0")
 
+def mostrar_menu():
+    print("=======Menu=======")
+    print("1. Agregar Producto")
+    print("2. Mostrar Producto")
+    print("3. Eliminar Producto")
+    print("4. Actualizar Disponibilidad")
+    print("5. Mostrar Productos")
+    print("6. Salir")
+
+def leer_opcion():
+    try:
+        opcion = int(input("Ingrese opción: \n"))
+        if opcion > 0 and opcion <= 6:
+            return opcion
+        else:
+            print("Opción debe estar entre 1 y 6")
+    except:
+        print("Valor debe ser numérico")
+
 def agregar_producto(lista_productos):
     while True:
-        nombre = input("Ingrese nombre del producto: \n").split()
+        nombre = input("Ingrese nombre del producto: \n").strip()
         if _validar_str(nombre):
             break
     while True:
         try:
             stock = int(input("Ingrese stock del producto: \n"))
-            if _validar_int == True:
+            if _validar_int(stock) == True:
                 break
         except:
             print("El número ingresado no es un entero")
     while True:
         try:
             precio = float(input("Ingrese precio del producto: \n"))
-            if _validar_float == True:
+            if _validar_float(precio) == True:
                 break
         except:
             print("El número ingresado no es un entero")
@@ -53,5 +72,36 @@ def agregar_producto(lista_productos):
         "disponible" : False
     }
     # Agregamos el elemento a la lista
-    producto.append(lista_productos)
+    lista_productos.append(producto)
+    print("Producto Agregado")
     pausar()
+
+def buscar_producto(nombre):
+    for p in range(len(lista_productos)):
+        #si el nombre que busco existe en la lista
+        if lista_productos[p]["nombre"] == nombre:
+            #retornare la posicion en la que se encuentra 'EL producto'
+            return p
+    #si no encuentra ningun nombre, retornara -1
+    return -1
+
+def actualizar_disponibilidad(lista_productos):
+    for p in lista_productos:
+        if p["stock"] > 0:
+            p["disponible"] = True
+        else:
+            p["disponible"] = False
+    print("Lista actualizada")
+
+def mostrar_productos():
+    actualizar_disponibilidad(lista_productos)
+    print("=== LISTA DE PRODUCTOS ===")
+    for p in lista_productos:
+        print(f"Nombre: {p["nombre"]}")
+        print(f"Stock: {p["stock"]}")
+        print(f"Precio: {p["precio"]}")
+        if p["disponible"] == True:
+            print("Estado: Disponible")
+        else:
+            print("Estado: Sin Stock")
+        print("*********************************")
--- main.py
+++ main.py
@@ -1,3 +1,37 @@
 from functions import *
 
-agregar_producto(lista_productos)
+limpiar()
+while True:
+    mostrar_menu()
+    opcion = leer_opcion()
+    if opcion == 1:
+        agregar_producto(lista_productos)   
+    elif opcion == 2:
+        nombre = input("Ingrese nombre a buscar: \n").strip()
+        posicion = buscar_producto(nombre)
+        if posicion != -1:
+            print(f"producto encontrado en la posicion: {posicion}")
+            print(f"Nombre: {lista_productos[posicion]["nombre"]}")
+            print(f"Stock: {lista_productos[posicion]["stock"]}")
+            print(f"Precio: {lista_productos[posicion]["precio"]}")
+            if lista_productos[posicion]["disponible"] == True:
+                print("Estado: Disponible")
+            else:
+                print("Estado: Sin Stock")
+        else:
+            print("No se encontraron productos")
+    elif opcion == 3:
+        nombre = input("Ingrese nombre a eliminar: \n").strip()
+        posicion = buscar_producto(nombre)
+        if posicion >= 0:
+            lista_productos.pop(posicion)
+            print("Producto Eliminado")
+        else:
+            print(f"El producto {nombre} no se encuentra registrado")
+    elif opcion == 4:
+        actualizar_disponibilidad(lista_productos)
+    elif opcion == 5:
+        mostrar_productos()
+    elif opcion == 6:
+        print("“Gracias por usar el sistema. Vuelva Pronto”")
+        break
